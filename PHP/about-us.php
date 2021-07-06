1b4cd49d0458e8e4bd9dce9b297960482de784e4
saurabh357 <saurabh357@gmail.com>
2021-07-06 19:30:54 +0530

    online

---
+++ PHP/about-us.php
@@ -0,0 +1,35 @@
+<?php
+include ("header.php");
+?>
+
+<div class="breadcrumb-area gray-bg">
+            <div class="container">
+                <div class="breadcrumb-content">
+                    <ul>
+                        <li><a href="<?php echo FRONT_SITE_PATH?>shop">Home</a></li>
+                        <li class="active">About us </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="about-us-area pt-50 pb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-7 d-flex align-items-center">
+                        <div class="overview-content-2">
+                            <h2>Welcome To <span>Food Ordering</span> Store !</h2>
+                            <p>To ensure and focus on better consumer experience, Foody has launched a platform for partners, wherein Foody partners can order premium packaging material at a reasonable cost and help us in providing an awesome experience for the consumers.</p>
+                            <p>The technology that delivers great food right to your doorstep was completely developed in-house!</P>
+
+
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+<?php
+include("footer.php");
+?>
---
+++ PHP/database.inc.php
@@ -0,0 +1,4 @@
+<?php
+date_default_timezone_set('Asia/Kolkata');
+$con=mysqli_connect('localhost:3306','root','','online_food');
+?>
